e031b74a3cac255e62f7f80afd8e5033b65564da
minoru horiuchi <noru238@gmail.com>
2024-02-22 17:20:59 +0900

    初期表示修正・prettier導入

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,28 +1,31 @@
-import Link from "next/link";
-import { getList } from "@/app/_libs/microcms";
+import Image from 'next/image'
+import Link from 'next/link'
+import { getList } from '@/app/_libs/microcms'
 
 export default async function StaticPage() {
-    const { contents } = await getList();
+  const { contents } = await getList()
 
-    // ページの生成された時間を取得
-    const time = new Date().toLocaleString();
+  if (!contents || contents.length === 0) {
+    return <h1>No contents</h1>
+  }
 
-    if (!contents || contents.length === 0) {
-        return <h1>No contents</h1>;
-    }
-
-    return (
-        <div>
-            <h1 className={'text-sm'}>{time}</h1>
-            <ul>
-                {contents.map((post) => {
-                    return (
-                        <li key={post.id}>
-                            <Link href={`/static/${post.id}`}>{post.title}</Link>
-                        </li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul>
+        {contents.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/static/${post.id}`}>{post.title}</Link>
+              <Image
+                src={post.eyecatch?.url ?? ''}
+                alt={'article image'}
+                width={300}
+                height={200}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
